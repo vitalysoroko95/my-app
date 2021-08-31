0577dee3dc1ccbd5f5b27724ab7b385dfa2c3948
DESKTOP-RE8LH2A\Shprgd <isephar@gmail.com>
2021-08-31 19:48:14 +0300

    31.08.2021 Unit tests (jest)

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,6 +3,7 @@ import {profileAPI, usersAPI} from "../api/api";
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
+const DELETE_POST = 'DELETE_POST';
 
 let initialState = {
     postsData: [
@@ -24,6 +25,12 @@ const profileReducer = (state = initialState, action) => {
                 postsData: [...state.postsData, newText]
             }
 
+            case DELETE_POST:
+            return {
+                ...state,
+                postsData: state.postsData.filter(p => p.id !== action.postId)
+            }
+
         case SET_USER_PROFILE: {
             return {
                 ...state,
@@ -43,6 +50,7 @@ const profileReducer = (state = initialState, action) => {
 
 
 export const addPostActionCreator = (newPostBody) => ({type: ADD_POST, newPostBody});
+export const deletePost = (postId) => ({type: DELETE_POST, postId});
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
 export const setStatus = (status) => ({type: SET_STATUS, status});
 
---
+++ src/redux/profile-reducer.test.js
@@ -0,0 +1,41 @@
+import {render, screen} from "@testing-library/react";
+import App from "../App";
+import profileReducer, {addPostActionCreator, deletePost} from "./profile-reducer";
+
+
+let state = {
+    postsData: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'My first post', likesCount: 23},
+        {id: 3, message: 'My first post', likesCount: 23},
+    ]
+
+};
+
+
+it('length postsData should be incremented', () => {
+    let action = addPostActionCreator("NewTestTextBody");
+    let newState = profileReducer(state, action);
+    expect(newState.postsData.length).toBe(4)
+
+});
+
+
+it('message of new post  should be NewTestTextBody', () => {
+    let action = addPostActionCreator("NewTestTextBody");
+    let newState = profileReducer(state, action);
+    expect(newState.postsData[3].message).toBe("NewTestTextBody")
+});
+
+it('after deleting length of messages should be decrement', () => {
+    let action = deletePost(1);
+    let newState = profileReducer(state, action);
+    expect(newState.postsData.length).toBe(2)
+});
+
+
+it(`after deleting length  shouldn't be decrement if id is incorrect`, () => {
+    let action = deletePost(100);
+    let newState = profileReducer(state, action);
+    expect(newState.postsData.length).toBe(3)
+});
